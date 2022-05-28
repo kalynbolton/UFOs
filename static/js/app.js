b323897bb966a698d91ff322945dd216606ab3f2
Kalyn Bolton <kalynbolton@yahoo.com>
2022-05-28 13:03:27 -0500

    Lesson 5.2 push

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,20 @@
 const tableData = data;
 // Reference the HTML table using D3
 var tbody = d3.select("tbody");
+
+// create new function for building a table and clear existing data
+function buildTable(data) {
+    tbody.html("");
+
+
+}
+
+// Add the forEach Function
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+    }
+  );
+});
